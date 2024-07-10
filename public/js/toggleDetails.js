29cd5e1c8b74eaf7accea987276c232766b5f2a2
Travis <datravousodds@gmail.com>
2024-07-09 21:33:15 -0500

    toggle logic implemented for prod details

--- public/js/toggleDetails.js
+++ public/js/toggleDetails.js
@@ -1,1 +1,19 @@
-console.log("hwll")
+console.log("Hello toggle details");
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelectorAll(".detail-subheader").forEach(function (expandBtn) {
+    expandBtn.addEventListener("click", function () {
+      const detailText =
+        this.closest(".detail-container").querySelector(".detail-text");
+      if (detailText.classList.contains("expanded")) {
+        detailText.classList.remove("expanded");
+        // detailText.style.display = "block";
+        this.querySelector("i").classList.remove("fa-minus");
+        this.querySelector("i").classList.add("fa-plus");
+      } else {
+        detailText.classList.add("expanded");
+        this.querySelector("i").classList.remove("fa-plus");
+        this.querySelector("i").classList.add("fa-minus");
+      }
+    });
+  });
+});
