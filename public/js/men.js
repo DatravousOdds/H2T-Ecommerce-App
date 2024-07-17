86c60b55debe2d4ed420e18e1cca538dcdd01b7a
Travis <datravousodds@gmail.com>
2024-07-16 21:20:01 -0500

    dd menu

---
+++ public/js/men.js
@@ -0,0 +1,16 @@
+const toggleDropdown = () => {
+  document.querySelector(".sort-content").classList.toggle("show");
+};
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = (event) => {
+  if (!event.target.matches(".dropdown-btn")) {
+    let dropdowns = document.getElementsByClassName("sort-content");
+    for (let i = 0; i < dropdowns.length; i++) {
+      let openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
+      }
+    }
+  }
+};
