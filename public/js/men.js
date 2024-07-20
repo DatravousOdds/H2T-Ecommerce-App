fd0af9a824ca1c21708ac6d6bb85c6f6d77ccf2b
Travis <datravousodds@gmail.com>
2024-07-20 13:57:52 -0500

    Sort By implemented completed!

--- public/js/men.js
+++ public/js/men.js
@@ -1,9 +1,11 @@
+const sortSelect = document.getElementById("sort-select");
+const sortIcon = document.getElementById("sort-icon");
+
 const sortBtn = document.getElementById("sort-btn");
 
 const toggleDropdown = () => {
   document.querySelector(".sort-content").classList.toggle("show");
-  document.getElementById("sort-icon").classList.toggle("rotate-down")
-  
+  sortIcon.classList.toggle("rotate-down");
 };
 
 // Close the dropdown menu if the user clicks outside of it
@@ -12,7 +14,7 @@ window.onclick = (event) => {
     let dropdowns = document.getElementsByClassName("sort-content");
     for (let i = 0; i < dropdowns.length; i++) {
       let openDropdown = dropdowns[i];
-      console.log(openDropdown[1]);
+
       if (openDropdown.classList.contains("show")) {
         openDropdown.classList.remove("show");
       }
@@ -20,7 +22,15 @@ window.onclick = (event) => {
   }
 };
 
-sortBtn.addEventListener("click", toggleDropdown);
-
+sortBtn.addEventListener("click", (event) => {
+  event.stopPropagation();
+  toggleDropdown();
+});
 const sortOption = document.querySelectorAll(".sort-content a");
-console.log(sortOption);
+// console.log(sortOption);
+sortOption.forEach((link) => {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    sortSelect.textContent = this.textContent;
+  });
+});
