79e7cf7cf215566c7a0aa140bf4203d60f8699b4
Travis <datravousodds@gmail.com>
2024-07-20 13:15:18 -0500

    sort opt functionality

--- public/js/men.js
+++ public/js/men.js
@@ -2,6 +2,8 @@ const sortBtn = document.getElementById("sort-btn");
 
 const toggleDropdown = () => {
   document.querySelector(".sort-content").classList.toggle("show");
+  document.getElementById("sort-icon").classList.toggle("rotate-down")
+  
 };
 
 // Close the dropdown menu if the user clicks outside of it
@@ -10,6 +12,7 @@ window.onclick = (event) => {
     let dropdowns = document.getElementsByClassName("sort-content");
     for (let i = 0; i < dropdowns.length; i++) {
       let openDropdown = dropdowns[i];
+      console.log(openDropdown[1]);
       if (openDropdown.classList.contains("show")) {
         openDropdown.classList.remove("show");
       }
@@ -18,3 +21,6 @@ window.onclick = (event) => {
 };
 
 sortBtn.addEventListener("click", toggleDropdown);
+
+const sortOption = document.querySelectorAll(".sort-content a");
+console.log(sortOption);
