c5ec55e474004c5ed11ec55a25dca6c6412c3233
Travis <datravousodds@gmail.com>
2024-09-08 17:45:37 -0500

    javascript logic implement for small filter menu and transition added

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,18 +1,22 @@
-// const toggle = document.getElementById("dropdown-toggle");
-
 const dropdownSection = document.querySelectorAll(".dropdown-section");
+const smallDropdownSection = document.querySelectorAll(".dropdown-section-sm");
+
+// testing
+smallDropdownSection.forEach((section) => [
+  console.log("Small Menus", section),
+]);
 
 /* 
     Selects all elements with class .dropdown-section 
     Loops through each dropdown-section to apply logic 
 */
 dropdownSection.forEach((section) => {
-
+  console.log("Dropdown Menu", dropdownSection);
+  console.log(section);
   const dropdownIcon = section.querySelector(".dropdown-icon i");
   const menu = section.querySelector(".dropdown-menu");
-  const toggle = section.querySelector(".statement-header");
-  
-  
+  const toggle = section.querySelector(".dropdown-header");
+
   toggle.addEventListener("click", () => {
     // Hide all other dropdown menus
     document.querySelectorAll(".dropdown-menu").forEach((otherMenu) => {
@@ -27,7 +31,6 @@ dropdownSection.forEach((section) => {
     // Toggle the clicked dropdown menu
     menu.classList.toggle("active");
 
-
     // Toggle rotation
     if (menu.classList.contains("active")) {
       dropdownIcon.style.transform = "rotate(180deg)";
@@ -37,3 +40,19 @@ dropdownSection.forEach((section) => {
   });
 });
 
+// year filter dropdown
+const yearHeader = document.querySelector(".dropdown-header");
+const yearIcon = yearHeader.querySelector(".dropdown-icon i");
+const yearMenu = document.querySelector(".year-dropdown");
+
+yearHeader.addEventListener("click", () => {
+  // Toggle the click dropdown menu
+  yearMenu.classList.toggle("open");
+
+  // Toggle rotation
+  if (yearMenu.classList.contains("open")) {
+    yearIcon.style.transform = "rotate(180deg)";
+  } else {
+    yearIcon.style.transform = "rotate(0deg)";
+  }
+});
