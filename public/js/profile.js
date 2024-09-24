34332d73c3187d0d509fdfae353073885246b4bb
Travis <datravousodds@gmail.com>
2024-09-24 02:32:32 -0500

    save button logic & edit button logic implemented for forms

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,18 +1,12 @@
 /* * 09/22/2024 Travis Odds  Adding action to forms, no connect db yet. * */
 
-
 const dropdownSection = document.querySelectorAll(".dropdown-section");
 const smallDropdownSection = document.querySelectorAll(".dropdown-section-sm");
-const spanElement = document.getElementById('filter-icon');
-const profileForms =  document.querySelectorAll(".profile-section");
-const hasFilterIcon = spanElement.querySelector('i.fa-filter') !== null;
-const firstSection = profileForms[0];
+const spanElement = document.getElementById("filter-icon");
+const profileSection = document.querySelectorAll(".profile-section");
+const hasFilterIcon = spanElement.querySelector("i.fa-filter") !== null;
+const firstSection = profileSection[0];
 const bio = firstSection.querySelector("#bio");
-
-
-
-
-
 
 /* 
     Selects all elements with class .dropdown-section 
@@ -52,117 +46,128 @@ dropdownSection.forEach((section) => {
 
 // Performs small menu action for profile -- payment information --
 smallDropdownSection.forEach((smallMenu) => {
-  const yearHeader = smallMenu.querySelector(".dropdown-header")
-  const yearIcon = smallMenu.querySelector(".dropdown-icon i")
-  const yearMenu = smallMenu.querySelector(".year-dropdown")
+  const yearHeader = smallMenu.querySelector(".dropdown-header");
+  const yearIcon = smallMenu.querySelector(".dropdown-icon i");
+  const yearMenu = smallMenu.querySelector(".year-dropdown");
 
   yearHeader.addEventListener("click", () => {
-  // Toggle the click dropdown menu
-  yearMenu.classList.toggle("open");
+    // Toggle the click dropdown menu
+    yearMenu.classList.toggle("open");
 
-  // Toggle rotation
-  if (yearMenu.classList.contains("open")) {
-    yearIcon.style.transform = "rotate(180deg)";
-  } else {
-    yearIcon.style.transform = "rotate(0deg)";
-  }
+    // Toggle rotation
+    if (yearMenu.classList.contains("open")) {
+      yearIcon.style.transform = "rotate(180deg)";
+    } else {
+      yearIcon.style.transform = "rotate(0deg)";
+    }
+  });
 });
 
-})
-
 // Performs menu actions such as save, cancel or edit for profile -- profile --
-profileForms.forEach(section => {
-  const editBtn = section.querySelectorAll('.edit-btn')
-  const actionBtns = section.querySelectorAll(".action-buttons");
-  actionBtns.forEach(btn => {{
-    btn.style.display = "none";
-  }})
-  
-})
-  
- 
- 
-  
-  // Initially hide save and cancel buttons
-  // saveBtn.style.display = "none";
-  // cancelBtn.style.display = "none";
- 
+profileSection.forEach((section) => {
+  const edit = section.querySelectorAll(".edit-info-header");
+  const allActionButtons = section.querySelectorAll(".action-buttons");
+  const saveBtn = section.querySelectorAll(".save-btn");
+  const cancelBtn = section.querySelectorAll(".cancel-btn");
+  const inputs = section.querySelectorAll("input");
+
+  // Handle save button click
+  saveBtn.forEach((btn) => {
+    console.log(btn);
+    btn.addEventListener("click", () => {
+      console.log("save button was click");
+      inputs.forEach((input) => {
+        input.disabled = true;
+        input.style.backgroundColor = "transparent";
+        input.style.border = "none";
+        input.style.boxShadow = "none";
+        input.style.padding = "0px 0px";
+      });
+      allActionButtons.forEach((action) => {
+        action.style.display = "none";
+      });
+    });
+  });
+
+  // Handle edit button click
+  edit.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      console.log("edit button was clicked!");
+
+      inputs.forEach((input) => {
+        input.disabled = false;
+        input.style.backgroundColor = "#e0e0e0";
+        input.style.border = "#b0b0b0";
+        input.style.padding = "12px 20px";
+      });
+
+      allActionButtons.forEach((action) => {
+        action.style.display = "flex";
+      });
+    });
+  });
 
   // Handle cancel button click
-  // cancelBtn.addEventListener("click", () => {
-  //   inputs.forEach(input => {
-  //     input.disabled = true;
-  //     input.style.backgroundColor = "transparent";
-  //     input.style.border = "none";
-  //     input.style.boxShadow = "none";
-  //     input.style.padding = "0px 0px";
-  
-  //     saveBtn.style.display = "none";
-  //     cancelBtn.style.display = "none";
-  //     editBtn.style.display = "flex";
-  
-  //   })
-  // })
-  
-  // // Handle edit button click
-  // editBtn.addEventListener('click', () => {
-  //   console.log('Edit button clicked'); // testing 
-  //   inputs.forEach(input => {
-  //     input.disabled = false;
-  //     input.style.backgroundColor = "#e0e0e0";
-  //     input.style.border = "#b0b0b0";
-  //     input.style.padding = "12px 20px";
-  
-    
-      
-  //   });
-  //   saveBtn.style.display = "inline"
-  //   cancelBtn.style.display = "inline"
-  //   editBtn.style.display = "none"  
-  // });
-  
-  // action for when the save chgs  btn is clicked :: Personal Information 
-  // saveBtn.addEventListener("click", () => {
-  //   inputs.forEach(input =>  {
-  //     input.disabled = true;
-  //     input.style.backgroundColor = "transparent";
-  //     input.style.border = "none";
-  //     input.style.boxShadow = "none";
-  //     input.style.padding = "0px 0px";
-  
-  //     // Send to a database
-      
-  
-  //   })
-  //    //  removing the save, cancel btns from the dom
-  //   saveBtn.style.display = "none"
-  //   cancelBtn.style.display =  "none"
-  //   editBtn.style.display = "flex"
-  
-  
-  
-  
-  // }) 
+  cancelBtn.forEach((btn) => {
+    console.log(btn);
+  });
+  allActionButtons.forEach((action) => {
+    action.style.display = "none";
+  });
+});
 
- 
+// Initially hide save and cancel buttons
+// saveBtn.style.display = "none";
+// cancelBtn.style.display = "none";
 
-  
+// Handle cancel button click
+// cancelBtn.addEventListener("click", () => {
+//   inputs.forEach(input => {
+//     input.disabled = true;
+//     input.style.backgroundColor = "transparent";
+//     input.style.border = "none";
+//     input.style.boxShadow = "none";
+//     input.style.padding = "0px 0px";
 
+//     saveBtn.style.display = "none";
+//     cancelBtn.style.display = "none";
+//     editBtn.style.display = "flex";
 
-  
-    
+//   })
+// })
 
+// // Handle edit button click
+// editBtn.addEventListener('click', () => {
+//   console.log('Edit button clicked'); // testing
+//   inputs.forEach(input => {
+//     input.disabled = false;
+//     input.style.backgroundColor = "#e0e0e0";
+//     input.style.border = "#b0b0b0";
+//     input.style.padding = "12px 20px";
+
+//   });
+//   saveBtn.style.display = "inline"
+//   cancelBtn.style.display = "inline"
+//   editBtn.style.display = "none"
+// });
+
+// action for when the save chgs  btn is clicked :: Personal Information
+// saveBtn.addEventListener("click", () => {
+//   inputs.forEach(input =>  {
+//     input.disabled = true;
+//     input.style.backgroundColor = "transparent";
+//     input.style.border = "none";
+//     input.style.boxShadow = "none";
+//     input.style.padding = "0px 0px";
+
+//     // Send to a database
+
+//   })
+//    //  removing the save, cancel btns from the dom
+//   saveBtn.style.display = "none"
+//   cancelBtn.style.display =  "none"
+//   editBtn.style.display = "flex"
+
+// })
 
 // Performs menu actions for pfp-section bio-section for profile -- profile --
-
-
-
-
-
-
-
-
-
-
-
-
