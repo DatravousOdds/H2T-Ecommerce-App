efb851808c67f918d7c2be5503612149d018b3e5
Travis <datravousodds@gmail.com>
2024-11-21 17:40:13 -0600

    add dom structure for products section on selling tab

--- public/js/selling.js
+++ public/js/selling.js
@@ -1,11 +1,19 @@
-const sellingTabs = document.querySelectorAll(".selling-tabs .selling-tab");
-
+const sellingNavButtons = document.querySelectorAll(".nav-button");
+const sellingSections = document.querySelectorAll(".content-section");
+console.log("sellingNavButtons", sellingNavButtons);
+console.log("sellingSections", sellingSections);
 // Dashboard navigation
-sellingTabs.forEach((tab) => {
-  tab.addEventListener("click", () => {
+sellingNavButtons.forEach((button) => {
+  button.addEventListener("click", () => {
     // Remove active class from all other tab
-    sellingTabs.forEach((tab) => tab.classList.remove("active"));
+    sellingNavButtons.forEach((button) => button.classList.remove("active"));
+    sellingSections.forEach((section) => section.classList.remove("active"));
 
-    tab.classList.add("active");
+    // Add show class to the clicked section
+    const tabId = button.getAttribute("data-tab");
+    const tabToShow = document.getElementById(tabId);
+    tabToShow.classList.add("active");
+
+    button.classList.add("active");
   });
 });
