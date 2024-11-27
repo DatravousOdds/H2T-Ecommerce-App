349af1b61445ac4372c0a7ccd809e771303eac86
Datravous Odds <datravousodds@gmail.com>
2024-11-27 14:53:14 -0600

    compeleted purchasing nav javascript functionality

---
+++ public/js/purchases.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const purchaseTypeNav = document.querySelector(".purchase-type-nav");
+  const purchaseTypeItems = document.querySelectorAll(".purchase-type-item");
+  const purchaseContentSections = document.querySelectorAll(
+    ".purchases-content-section"
+  );
+
+  console.log("purchaseContentSections", purchaseContentSections);
+  purchaseTypeItems.forEach((item, index) => {
+    item.addEventListener("click", () => {
+      purchaseTypeItems.forEach((item) => item.classList.remove("active"));
+      item.classList.add("active");
+
+      purchaseContentSections.forEach((section) =>
+        section.classList.remove("active")
+      );
+      console.log("index", index);
+      purchaseContentSections[index].classList.add("active");
+    });
+  });
+});
