1d50cf3b67f6fb9213688e95fa273eb38e05b949
Datravous Odds <datravousodds@gmail.com>
2024-12-09 01:14:47 -0600

    update header

--- public/js/nav.js
+++ public/js/nav.js
@@ -4,32 +4,70 @@ const newNav = () => {
   nav.innerHTML = `
 
     
-  <nav class="navbar justify-content-between">
-  <div class="nav-items">
-    <!-- Search bar -->
-    <div class="search_box form-inline">
-      <form class="form-inline">
-        <input type="text" placeholder="Search here...." class="input_box">
-        <button type="submit" class="triggerSearch reg-btn"><i class="fa fa-search icon"></i></button>
-      </form>
-    </div>
-    <!-- end of search bar -->
+  <nav class="navbar" aria-label="main navigation">
+
+<!-- Left section with search and logo -->
+<div class="search-section">
+  <form class="search-form" role="search">
+  <button type="submit" class="search-button" aria-label="Search">
+     <i class="fa-solid fa-magnifying-glass"></i>
+    </button>
+    <input type="text" placeholder="Search here...." class="search-input" aria-label="Search Products">
     
-  </div>
-  <a href="/"><img src="../images/logoh2t_home.png" class="logo-img" width="80px" height="50px" /></a>
-  <div id="menu">
-    <button class="close-btn"><i class="fa-solid fa-x"></i></button>
-    <ul class="list">
-      <li class="sub-header"><a href="/mens" class="sh sub-btn">Men</a></li>
-      <li class="sub-header"><a href="/women" class="sh sub-btn">Women</a></li>
-      <li class="sub-header"><a href="/accessories" class="sh sub-btn">Accessories</a></li>
-      <li class="sub-header"><a href="/seller" class="sh sub-btn">Sell</a>
-      <li><button class="login-btn"><a class='center-text' href="/login">Login</a></button></li>
-      <li><button class="signup-btn"><a class='center-text' href=/signup>Signup</a></button></li>
-      <li><a href="#"><i class="fa-regular fa-bell"></i></a></li>
-      <li><a href="/cart" class="sh"><i class="fa-solid fa-bag-shopping"></i></a></li>
-      <li id="user_profile" class="hide"><a href="/profile"><i class="fa-regular fa-user user_profile"></i></a></liv>
-    </ul>
+  </form>
+
+  <a href="/"><img src="../images/logoh2t_home.png" class="logo-img" width="80px" height="50px"/></a>
+</div>
+
+<!-- Mobile menu button -->
+<button class="menu-button" aria-label="Menu"
+        aria-expanded="false"
+        aria-controls="main-menu">
+  <i class="fa-solid fa-bars"></i>
+</button>
+
+
+<!-- Navigation menu -->
+<div class="nav-menu" id="main-menu" aria-hidden="true">
+    <button class="close-btn" aria-label="Close Menu" aria-controls="main-menu">
+      <i class="fa-solid fa-x"></i>
+    </button>
+ 
+  
+    <ul class="nav-list">
+            <li><a href="/mens" class="nav-link">Men</a></li>
+            <li><a href="/women" class="nav-link">Women</a></li>
+            <li><a href="/accessories" class="nav-link">Accessories</a></li>
+            <li><a href="/seller" class="nav-link">Sell</a></li>
+            <li>
+                <a href="/login" class="button-link login-button">Login</a>
+            </li>
+            <li>
+                <a href="/signup" class="button-link signup-button">Signup</a>
+            </li>
+            <li>
+                <a href="#notifications" 
+                   class="nav-link" 
+                   aria-label="Notifications">
+                    <i class="fa-regular fa-bell"></i>
+                </a>
+            </li>
+           <li>
+    <a href="/cart" 
+       class="nav-link cart-link" 
+       aria-label="Shopping cart">
+        <i class="fa-solid fa-cart-shopping"></i>
+        <span class="cart-amount" aria-label="Cart items">0</span>
+    </a>
+</li>
+            <li>
+                <a href="/profile" 
+                   class="nav-link" 
+                   aria-label="User profile">
+                    <i class="fa-regular fa-user"></i>
+                </a>
+            </li>
+      </ul>
   </div>
 </nav>
 `;
