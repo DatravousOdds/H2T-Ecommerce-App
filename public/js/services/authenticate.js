3fe5c7c646bb5cc7cf7a0c8515441cb83df5ba2b
Datravous Odds <datravousodds@gmail.com>
2025-12-31 18:42:20 -0600

    feat: implemented vaildation for authentication steps, steps 1, and 2 - refactored vaildation for step 1, checks for image source and required count of images -  implemented vaildateForm function which takes in form type and vaildates based off rules - created global variable categorySelected so that vaildateForm has access

--- public/js/services/authenticate.js
+++ public/js/services/authenticate.js
@@ -1,5 +1,7 @@
 
 
+
+
 // Image upload functionality
 const imageInputs = document.querySelectorAll(".file-input");
 const imageItems = document.querySelectorAll(".image-item");
@@ -86,12 +88,14 @@ imageInputs.forEach((input) => {
 
 const categories = document.getElementById('categories');
 const dynamicFormContainer = document.getElementById('dynamic-form-container');
-
+let categorySelected;
 
 categories.addEventListener('change', (e) => {
   const target = e.target.value;
+  categorySelected = target;
+  
   // get form
-  formLocator(target);
+  formLocator(categorySelected);
 
 
 })
@@ -158,7 +162,6 @@ function updateProgressBar(currentStep) {
   root.style.setProperty('--progress-percentage', progressPercentage); 
 }
 
-
 function updateProgressSteps(currentStep) {
   const steps = document.querySelectorAll(".progress-steps .step");
 
@@ -178,25 +181,116 @@ function updateProgressSteps(currentStep) {
   });
 }
 
+function vaildateForm(form) {
+ 
+    const vaildationRules = {
+
+      'Trading Cards': 
+      [
+        {id: 'card-brand', name: 'Brand', required: true },
+        {id: 'card-name', name: 'Card Name', required: true },
+        {id: 'card-set', name: 'Set', required: true },
+        {id: 'card-year', name: 'Year', required: true, type: 'number' },
+        {id: 'card-condition', name: 'Condition', required: true },
+      ],
+      'Apparel': 
+      [
+        {id: 'apparel-brand', name: 'Brand', required: true, type: 'text'},
+        {id: 'apparel-type', name: 'Item Type', required: true },
+        {id: 'apparel-size', name: 'Size', required: true },
+        {id: 'apparel-condition', name: 'Condition', required: true },
+        {id: 'apparel-color', name: 'Color', required: true, type: 'text'},
+      ],
+      'Sneakers': 
+      [
+        {id: 'sneaker-brand', name: 'Brand', required: true, type: 'text'},
+        {id: 'sneaker-model', name: 'Model', required: true, type: 'text'},
+        {id: 'sneaker-size', name: 'Size', required: true },
+        {id: 'sneaker-condition', name: 'Condition', required: true },
+      ],
+      'Shoes':
+      [
+        {id: 'shoes-brand', name: 'Brand', required: true, type: 'text'},
+        {id: 'shoes-model', name: 'Model', required: true, type: 'text'},
+        {id: 'shoes-type', name: 'Shoe Type', required: true },
+        {id: 'shoes-color', name: 'Color', required: true, type: 'text' },
+        {id: 'shoes-condition', name: 'Condition', required: true },
+      ],
+      'Accessories': 
+      [
+        {id: 'accessories-type', name: 'Accessory Type', required: true },
+        {id: 'accessories-brand', name: 'Brand', required: true, type: 'text'},
+        {id: 'accessories-condition', name: 'Condition', required: true },
+      ]
+    }
+
+    const rules = vaildationRules[form];
+
+    const errors = [];
+
+    rules.forEach(rule => {
+
+      const element = document.getElementById(rule.id);
+
+      if (!element) {
+        console.log('No element found!');
+      }
+
+      const value = element.value;
+
+      if(!value && rule.required) {
+        errors.push(`${rule.name} is required`);
+        return;
+      }
+
+      if (rule.type === 'number' && value && isNaN(value)) {
+        errors.push(`${rule.name} is must a number`);
+        return;
+      }
+
+    })
+
+    console.log(errors)
+    
+
+  
+}
 
 function validateStep(stepNumber) {
+  
   if (stepNumber === 1) {
-    // Validate image uploads
-    const uploadedImages = document.querySelectorAll(".image-preview[src]");
-    // console.log("Images Uploaded:", uploadedImages);
-    if (uploadedImages.length < 5) {
-      alert("Please upload all required images"); // turn into alert ui component later
+    const images = document.querySelectorAll('.image-preview');
+    const REQUIRED_IMAGES = 5;
+
+    let uploadedImages = 0;
+    let emptyIndexes = [];
+    
+    images.forEach((img, index) => {
+      const src = img.getAttribute('src');
+      if (src && src.trim() !== '') {
+        uploadedImages++;
+      } else {
+        emptyIndexes.push(index + 1);
+      }
+    });
+
+    if (uploadedImages < REQUIRED_IMAGES) {
+      alert(`Please upload ${REQUIRED_IMAGES} images, currently uploaded ${uploadedImages}`);
       return false;
     }
-  } else if (stepNumber === 2) {
-    // check type of form
 
-    // vaildate form
-
+    return true;
     
+  } else if (stepNumber === 2) {
+
+    vaildateForm(categorySelected);
+
+    return;
   }
   return true;
 }
+
+
 
 // Takes in category and display that form
 function formLocator(category) {
@@ -235,7 +329,13 @@ showStep(currentStep);
 
 // Add event listener to navigation buttons
 nextBtn.forEach((btn) => {
-  btn.addEventListener("click", nextStep);
+  btn.addEventListener("click", () => {
+    console.log("current step:", currentStep);
+    if (validateStep(currentStep)) {
+      nextStep();
+    }
+    return;
+  });
 });
 
 backBtn.forEach((btn) => {
