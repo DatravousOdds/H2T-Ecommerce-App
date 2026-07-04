3af232f72bf06edaf4e453277100fdab908aa80d
Datravous Odds <datravousodds@gmail.com>
2026-07-03 21:47:51 -0500

    feat: Add Firebase configuration file and update styles for hero section

---
+++ public/js/core/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  // Your web app's Firebase configuration
+  apiKey: "AIzaSyBY2UnjxAeO344Q0L841KybRcgk_VztUzo",
+  authDomain: "ecom-website-94d87.firebaseapp.com",
+  projectId: "ecom-website-94d87",
+  storageBucket: "ecom-website-94d87.appspot.com",
+  messagingSenderId: "786012301650",
+  appId: "1:786012301650:web:cc60aa81cadff2a561728b"
+};
+
+export { firebaseConfig };
